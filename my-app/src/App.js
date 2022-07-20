1d9907bad49cba3f2088c1772406b6aee5f0a6cb
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-07-20 11:56:41 -0500

    navbar working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar'
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
-import { Navbar } from './components';
 import './App.css';
 
 const App = () => (
   <div>
-    <Navbar />
+    <Navbar/>
   </div>
 );
 
