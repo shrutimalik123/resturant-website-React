e3357c77e1d9933c3b2a5e678c5257eee121b859
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-07-22 17:07:07 -0500

    about us

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar'
 import Header from './container/Header/Header';
+import AboutUs from './container/AboutUs/AboutUs';
 import './App.css';
 
 const App = () => (
   <div>
     <Navbar/>
     <Header/>
+    <AboutUs/>
   </div>
 );
 
